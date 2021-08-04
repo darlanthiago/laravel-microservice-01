95d876cc31bbcdbf7411a271f8c856c15a1888c5
Darlan Thiago <tdarlan1@hotmail.com>
2021-08-04 10:49:46 -0300

    Add Categories Resource Route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\{
+    CategoryController
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::resource('categories', CategoryController::class);
+
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'success',
+        'application' => env("APP_NAME"),
+    ]);
 });
