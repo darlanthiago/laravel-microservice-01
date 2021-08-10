5eaa9f972a7adec2c3d420940d15a6aca7e2e06a
Darlan Thiago <tdarlan1@hotmail.com>
2021-08-10 13:01:26 -0300

    Add Image to Return

--- app/Http/Resources/CompanyResource.php
+++ app/Http/Resources/CompanyResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class CompanyResource extends JsonResource
 {
@@ -20,6 +21,7 @@ class CompanyResource extends JsonResource
             'name' => $this->name,
             'category' => new CategoryResource($this->category),
             'url' => $this->url,
+            'image' => Storage::url($this->image),
             'email' => $this->email,
             'phone' => $this->phone,
             'whatsapp' => $this->whatsapp,
